b1e9f88b36e340ac4259c6c6ad9e11d6a4d445c4
Aadish Jain <aadishhere@gmail.com>
2025-05-15 10:49:23 +0530

    Initial Commit

---
+++ SplashScreen/SplashScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplashScreenApp.swift
+//  SplashScreen
+//
+//  Created by Aadish Jain on 15/05/25.
+//
+
+import SwiftUI
+
+@main
+struct SplashScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
